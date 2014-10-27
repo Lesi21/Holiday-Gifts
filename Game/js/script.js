a18c588893d8138a3ad28b076d4b1d3350fd23b2
xadjek <xadjek@rambler.ru>
2014-10-27 17:15:43 +0300

    движение добавлено

---
+++ Game/js/script.js
@@ -0,0 +1,9 @@
+function preload() {
+	game.load.image('spaceship', 'images/spaceship.png');
+	game.load.image('house', 'images/house.jpg');
+	game.load.image('tree', 'images/tree.jpg');
+	game.load.image('rain', 'images/rain.jpg');
+	game.load.image('background', 'images/background.png');
+	game.load.image('snow', 'images/snow.jpg');
+    game.load.image('star', 'images/star.png');
+}
